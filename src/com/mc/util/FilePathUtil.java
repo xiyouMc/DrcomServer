a92ef0204ad3cc09abce11dc701828ae90295011
xiyouMc <694449212@qq.com>
2014-12-12 22:06:11 +0800

    服务器稳定版发布

--- src/com/mc/util/FilePathUtil.java
+++ src/com/mc/util/FilePathUtil.java
@@ -2,10 +2,13 @@ package com.mc.util;
 
 public class FilePathUtil {
 
-	 public static final String tomcatPath = "E:\\apache-tomcat-6.0.26\\webapps\\Drcom";
-	 public static final String htmlPath  = tomcatPath + "\\a.html";
-	 public static final String picPath = "http://222.24.63.101/Drcom/2.jpg";
-	/* public static final String tomcatPath = "E:\\apache-tomcat-6.0.30\\apache-tomcat-6.0.30\\webapps\\Drcom";
-	 public static final String htmlPath  = tomcatPath + "\\a.html";
-	 public static final String picPath = "http://localhost:8080/Drcom/2.jpg";*/
+	public static final String tomcatPath = "E:\\apache-tomcat-8.0.9-windows-x64\\apache-tomcat-8.0.9\\webapps\\Drcom";
+	public static final String htmlPath = tomcatPath + "\\a.html";
+	public static final String picPath = "http://222.24.63.101/Drcom/2.jpg";
+	/*
+	 * public static final String tomcatPath =
+	 * "E:\\apache-tomcat-6.0.30\\apache-tomcat-6.0.30\\webapps\\Drcom"; public
+	 * static final String htmlPath = tomcatPath + "\\a.html"; public static
+	 * final String picPath = "http://localhost:8080/Drcom/2.jpg";
+	 */
 }
